afcdd3662b2329f91f84642bfad67e9a40254bca
Цомкалов Игорь <igorcomkalov@gmail.com>
2024-05-13 09:06:06 +0300

    Добавьте файлы проекта.

---
+++ Povtor.slova/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string inputString = Console.ReadLine();
+
+        string outputString = HighlightRepeatedWords(inputString);
+
+        Console.WriteLine(outputString);
+    }
+
+    static string HighlightRepeatedWords(string input)
+    {
+        // Регулярное выражение для поиска повторяющихся слов
+        string pattern = @"\b(\w+)\b(?=.*\b\1\b)";
+
+        // Заменяем найденные слова на выделенные тегами <strong>
+        string result = Regex.Replace(input, pattern, "<strong>$1</strong>");
+
+        return result;
+    }
+}
